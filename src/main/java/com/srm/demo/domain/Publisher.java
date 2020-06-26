1dacf264024139308fb83c7a9e2c39f553811c37
jfspps <jfsapps@gmail.com>
2020-06-26 15:52:54 +0100

    added books and authors Thymeleaf views

--- src/main/java/com/srm/demo/domain/Publisher.java
+++ src/main/java/com/srm/demo/domain/Publisher.java
@@ -11,6 +11,8 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String name;
     private String addressLine1;
     private String city;
     private String state;
@@ -25,7 +27,8 @@ public class Publisher {
     public Publisher() {
     }
 
-    public Publisher(String addressLine1, String city, String state, String zipCode) {
+    public Publisher(String name, String addressLine1, String city, String state, String zipCode) {
+        this.name = name;
         this.addressLine1 = addressLine1;
         this.city = city;
         this.state = state;
@@ -38,6 +41,14 @@ public class Publisher {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAddressLine1() {
@@ -97,6 +108,7 @@ public class Publisher {
     public String toString() {
         return "Publisher{" +
                 "id=" + id +
+                ", name='" + name + '\'' +
                 ", addressLine1='" + addressLine1 + '\'' +
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
