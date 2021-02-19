36c64f03e4838af2cf0a4dac9ee8b1a90e57af24
jfspps <jfsapps@gmail.com>
2021-02-19 22:50:45 +0000

    clean-up

--- src/main/java/com/srm/demo/repositories/AuthorRepository.java
+++ src/main/java/com/srm/demo/repositories/AuthorRepository.java
@@ -3,8 +3,5 @@ package com.srm.demo.repositories;
 import com.srm.demo.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 12/23/19.
- */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
